0415c5bd19f7d9fd84b27c3cf81a375e82217e3b
jinwoo <wwo1278@gmail.com>
2022-02-24 15:05:50 +0900

    사용자 이름 저장

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-const ttt = document.querySelectorAll(".hello h1:first-child")
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-console.log(ttt);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();//
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    // show the form
+} else {
+    // show the greeting
+}
